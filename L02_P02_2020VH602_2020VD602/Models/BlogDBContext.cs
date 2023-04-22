d797ce3f86898ca7df0815886618a1e9d59775b5
migue <migue@LAPTOP-FKTUUEN1>
2023-04-22 09:36:42 -0600

    Terminado

--- L02_P02_2020VH602_2020VD602/Models/BlogDBContext.cs
+++ L02_P02_2020VH602_2020VD602/Models/BlogDBContext.cs
@@ -8,18 +8,11 @@ namespace L02_P02_2020VH602_2020VD602.Models
         {
 
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        public DbSet<Usuarios> Usuarios { get; set; }
 
+        public DbSet<Usuarios> Usuarios { get; set; }
         public DbSet<Publicaciones> Publicaciones { get; set; }
-=======
         public DbSet<comentarios> comentarios { get; set; }
         public DbSet<calificaciones> calificaciones { get; set; }
->>>>>>> 7eaf252f1abfe50c8b5051bf76a095f383774e0d
-=======
-        public DbSet<comentarios> comentarios { get; set; }
-        public DbSet<calificaciones> calificaciones { get; set; }
->>>>>>> 7eaf252f1abfe50c8b5051bf76a095f383774e0d
+
     }
 }
