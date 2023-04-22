489ccbe89b2f5585c156802b7903f28944aa815a
migue <migue@LAPTOP-FKTUUEN1>
2023-04-22 08:43:51 -0600

    Agregar archivos de proyecto.

---
+++ L02_P02_2020VH602_2020VD602/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02_P02_2020VH602_2020VD602.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
